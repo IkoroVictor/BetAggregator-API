ff53d68cd9bbc9fa76e9eecac8e023fbc9ee6fa0
ikorovictor <ikoro.victor@gmail.com>
2015-06-14 08:57:18 +0100

    Initial Commit

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-worker: node --expose-gc start_service.js
-worker2: node --expose-gc workers/worker1.js
-worker3: node --expose-gc workers/worker2.js
+web: node index.js
--- index.js
+++ index.js
@@ -9,6 +9,30 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
+app.get('/days', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/games/:day', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/game/:id', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/games/search', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/game/:id/codes', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/games/:day/codes', function (req, res) {
+    res.send('Hello World')
+})
+
 
 
 var port = process.env.PORT || 3000;
